f265b7693fdf3ac7a80387287930da31a9dccc99
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-04-13 10:09:53 +0300

    Added BrowserRouter, Layout, Footer and Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import { HeaderWrapper, HeaderTitle } from './styled';
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <HeaderTitle>Распределение нагрузки на кафедры</HeaderTitle>
+    </HeaderWrapper>
+  );
+}
+
+export default Header;

ee8c97aee40e5190f897fb3d148209bd9ccad2c4
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-05-13 09:18:03 +0300

    worked with agGrid and fake data

---
+++ src/pages/MainTable/index.js
@@ -0,0 +1,46 @@
+import { useState, useMemo, useCallback, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+import { tableHeaders, fakeRowData } from './static';
+
+function MainTable() {
+  const gridRef = useRef();
+  const [rowData] = useState(fakeRowData);
+
+  const [columnDefs] = useState(tableHeaders);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      resizable: true,
+    };
+  }, []);
+
+  const autoSizeAll = useCallback((skipHeader) => {
+    const allColumnIds = [];
+    gridRef.current.columnApi.getColumns().forEach((column) => {
+      allColumnIds.push(column.getId());
+    });
+    gridRef.current.columnApi.autoSizeColumns(allColumnIds, skipHeader);
+  }, []);
+
+  const onGridReady = useCallback(() => {
+    autoSizeAll(false);
+  }, [autoSizeAll]);
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 600 }}>
+      <AgGridReact
+        ref={gridRef}
+        defaultColDef={defaultColDef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        onGridReady={onGridReady}
+      ></AgGridReact>
+    </div>
+  );
+}
+
+export default MainTable;

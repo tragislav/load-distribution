dc52fdf4a0019554cbec908b83caa11e7e308f6a
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-06-07 15:57:49 +0300

    Worked with modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -14,10 +15,65 @@ const customStyles = {
 };
 
 function MyModal({ data, isOpen, onRequestClose }) {
+  const [numInputs, setNumInputs] = useState(0);
+  const [inputValues, setInputValues] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const handleNumInputsChange = (e) => {
+    if (+e.target.value > 0) {
+      const newNumInputs = parseInt(e.target.value, 10);
+      setNumInputs(newNumInputs);
+      setInputValues(Array(newNumInputs).fill(0));
+    }
+  };
+
+  const handleInputChange = (e, index) => {
+    const newValue = parseInt(e.target.value, 10) || 0;
+    setInputValues((prevValues) => {
+      const newValues = [...prevValues];
+      newValues[index] = newValue;
+      return newValues;
+    });
+  };
+
+  const checkSum = () => {
+    const sum = inputValues.reduce((acc, val) => acc + val, 0);
+    if (sum === data.countStudenty) {
+      setMessage('Сумма введенных чисел равна ' + data.countStudenty);
+    } else {
+      setMessage('Сумма введенных чисел не равна ' + data.countStudenty);
+    }
+  };
+
+  useEffect(() => {
+    checkSum();
+  }, [inputValues]);
+
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} style={customStyles}>
       <button onClick={onRequestClose}>Закрыть</button>
-      <p>{data && data.idSpeciality}</p>
+      <p>{data && data.countStudenty}</p>
+      <div>
+        <label>
+          Количество инпутов:
+          <input
+            type="number"
+            value={numInputs}
+            onChange={handleNumInputsChange}
+          />
+        </label>
+        <div>
+          {Array.from({ length: numInputs }, (_, index) => (
+            <input
+              key={index}
+              type="number"
+              value={inputValues[index]}
+              onChange={(e) => handleInputChange(e, index)}
+            />
+          ))}
+        </div>
+        <p>{message}</p>
+      </div>
     </Modal>
   );
 }
